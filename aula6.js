6706ccaf1398cc226269eb61dfaaf34c63b46793
21seya <wallacenevesk9@outlook.com>
2020-03-22 19:13:39 -0300

    Enviado os arquivos

--- aula6.js
+++ aula6.js
@@ -2,3 +2,10 @@ const escola = 'Estacio'
 
 console.log(escola.charAt(6))
 console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('2'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0,3))
+
+console.log('Escola'.concat(escola).concat("!"))
+console.log(escola.replace(3,'e'))
---
+++ aula7.js
@@ -0,0 +1,11 @@
+const nome = 'Wallace'
+const concatenacao = 'Ola' + nome + '!'
+const template = `Olá ${nome}!`
+
+console.log(concatenacao,template)
+
+// expressões
+
+console.log(`1+1= ${1+1}`)
+
+const up = texto => texto.toUpperCase()
